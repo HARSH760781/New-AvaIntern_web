b5f8225c7ba78506efdf152069ab52bdfe055ca9
HARSH760781 <98482966+HARSH760781@users.noreply.github.com>
2025-03-08 18:07:36 +0530

    serve static file

--- Backend/server.js
+++ Backend/server.js
@@ -218,8 +218,15 @@ app.post("/create-payment-order", async (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, "dist")));
-
+app.use(
+  express.static(path.join(__dirname, "dist"), {
+    setHeaders: (res, filePath) => {
+      if (filePath.endsWith(".css")) {
+        res.setHeader("Content-Type", "text/css");
+      }
+    },
+  })
+);
 // Handle all other routes by serving the index.html file
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
