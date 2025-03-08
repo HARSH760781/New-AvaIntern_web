6fe42a652f8e39be74ed3db2c797d6790e6f1c6a
HARSH760781 <98482966+HARSH760781@users.noreply.github.com>
2025-03-08 17:05:44 +0530

    serve static file

--- Backend/server.js
+++ Backend/server.js
@@ -218,9 +218,11 @@ app.post("/create-payment-order", async (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "build")));
+
+// Handle all other routes by serving the index.html file
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
