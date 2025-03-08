645c0920e1c3c17c8b5e2ddaf1cdeb86666bc250
HARSH760781 <98482966+HARSH760781@users.noreply.github.com>
2025-03-08 16:31:07 +0530

    serve static file

--- Backend/server.js
+++ Backend/server.js
@@ -218,9 +218,9 @@ app.post("/create-payment-order", async (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, "..", "frontend", "dist")));
+app.use(express.static(path.join(__dirname, "dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
